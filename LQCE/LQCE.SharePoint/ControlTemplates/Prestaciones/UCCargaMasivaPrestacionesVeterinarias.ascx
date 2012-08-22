171fe373d5445db5614ffb037861a6f71396cfc5
proyectosweb <jgenovam@gmail.com>
2012-08-22 15:18:58 -0400

    srgdf
    dfghdh

--- LQCE/LQCE.SharePoint/ControlTemplates/Prestaciones/UCCargaMasivaPrestacionesVeterinarias.ascx
+++ LQCE/LQCE.SharePoint/ControlTemplates/Prestaciones/UCCargaMasivaPrestacionesVeterinarias.ascx
@@ -6,3 +6,4 @@
 <%@ Import Namespace="Microsoft.SharePoint" %> 
 <%@ Register Tagprefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
 <%@ Control Language="C#" AutoEventWireup="true" CodeBehind="UCCargaMasivaPrestacionesVeterinarias.ascx.cs" Inherits="LQCE.SharePoint.ControlTemplates.Prestaciones.UCCargaMasivaPrestacionesVeterinarias" %>
+<asp:Panel ID="jhjk" runat="server"></asp:Panel>
--- LQCE/LQCE.SharePoint/ControlTemplates/Prestaciones/UCCargaMasivaPrestacionesVeterinarias.ascx.designer.cs
+++ LQCE/LQCE.SharePoint/ControlTemplates/Prestaciones/UCCargaMasivaPrestacionesVeterinarias.ascx.designer.cs
@@ -1,6 +1,24 @@
-﻿namespace LQCE.SharePoint.ControlTemplates.Prestaciones
-{
-    public partial class UCCargaMasivaPrestacionesVeterinarias
-    {
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace LQCE.SharePoint.ControlTemplates.Prestaciones {
+    
+    
+    public partial class UCCargaMasivaPrestacionesVeterinarias {
+        
+        /// <summary>
+        /// Control jhjk.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel jhjk;
     }
 }
